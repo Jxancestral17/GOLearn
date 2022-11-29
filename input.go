0003805b7ca5448dba93cc63e980a4cda2096377
Federico Manfredi Longo <110201900+Jxancestral17@users.noreply.github.com>
2022-11-29 22:42:53 +0100

    Simple user input

---
+++ input.go
@@ -0,0 +1,24 @@
+// Return the Highest and Lowest number, start to array
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the number between 1 - 10: ")
+	numb, _ := reader.ReadString('\n')
+	numbex, err := strconv.Atoi(numb[:len(numb)-1])
+	if err != nil {
+		fmt.Println(err)
+	}
+	if numbex < 1 || numbex > 10 {
+		fmt.Println("The number is not between 1 and 10 ")
+	}else{
+    fmt.Println("Your number is ",numbex)
+  }
+}
